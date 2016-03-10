ac121b306f65f92b638c69f6391ff8b1300857c4
尤晟 <1197993367@qq.com>
2016-03-10 11:02:52 +0800

    3.10 11:00

--- app/src/main/java/com/example/yousheng/listview/MainActivity.java
+++ app/src/main/java/com/example/yousheng/listview/MainActivity.java
@@ -2,17 +2,38 @@ package com.example.yousheng.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] data= {"111","222","33","4","5","66","7","8","999","11","222","33","4","5","222","33","4","5",};
+//    private String[] data= {"111","222","33","4","5","66","7","8","999","11","222","33","4","5","222","33","4","5",};
+    private List<Fruit> fruitList= new ArrayList<Fruit>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void initFruits(){
+        fruitList.add(new Fruit("1",R.drawable.align_left));
+        fruitList.add(new Fruit("2",R.drawable.align_right));
+        for (int i=1;i<100;i++){
+        fruitList.add(new Fruit("hi!"+i,R.drawable.arrow_two_head));}
     }
 }
